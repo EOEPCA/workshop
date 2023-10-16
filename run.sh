acd97a07dafd85a9beb70528ff176dd4784e1376
Richard Conway <richard@rconway.co.uk>
2023-10-16 10:44:31 +0100

    Script tidy-up

--- run.sh
+++ run.sh
@@ -15,14 +15,22 @@ function onExit() {
 }
 trap "onExit" EXIT
 
-docker run --rm --name jupyterlab \
-  --user root \
-  -p 8888:8888 \
-  -v ${PWD}/workshop:/home/${NB_USER}/work \
-  -e NB_USER="${NB_USER}" \
-  -e NB_UID="${NB_UID}" \
-  -e NB_GID="${NB_GID}" \
-  -e CHOWN_HOME="yes" \
-  -e JUPYTER_ENABLE_LAB="yes" \
-  eoepca/workshop \
-  start-notebook.sh --NotebookApp.token=\'\'
+main() {
+  runWithDocker
+}
+
+runWithDocker() {
+  docker run --rm --name jupyterlab \
+    --user root \
+    -p 8888:8888 \
+    -v ${PWD}/workshop:/home/${NB_USER}/work \
+    -e NB_USER="${NB_USER}" \
+    -e NB_UID="${NB_UID}" \
+    -e NB_GID="${NB_GID}" \
+    -e CHOWN_HOME="yes" \
+    -e JUPYTER_ENABLE_LAB="yes" \
+    eoepca/workshop \
+    start-notebook.sh --NotebookApp.token=\'\'
+}
+
+main "$@"
